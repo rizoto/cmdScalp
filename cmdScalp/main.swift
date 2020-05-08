f94c184d4d69603cc07ba2cb7c3b9934f0c19f21
Lubor Kolacny <lubor@itroja.com.au>
2020-05-08 13:54:43 +1000

    Initial Commit

---
+++ cmdScalp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cmdScalp
+//
+//  Created by Lubor Kolacny on 8/5/20.
+//  Copyright © 2020 Lubor Kolacny. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
